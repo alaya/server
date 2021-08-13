95d894d379f77135db93a2792b02dc13568de4cd
alaya <lubashka-u@yandex.ru>
2021-08-13 13:24:31 +0400

    for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fittingroom.wsgi --log-file -
--- fittingRoom/settings.py
+++ fittingRoom/settings.py
@@ -11,20 +11,27 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%f(4_6#7-yo%mp@!h3f+g@o(dn3r_kdd(%4qacn)(swaw%r4^'
+# settings.py
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -84,6 +91,7 @@ WSGI_APPLICATION = 'fittingRoom.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import dj-database-url
 
 DATABASES = {
     'default': {
@@ -96,6 +104,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj-database-url.config()
+DATABASE['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
